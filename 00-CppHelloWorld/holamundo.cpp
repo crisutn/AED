4ffd44a7188f3dfc683ece17d3c1ed7078437a43
Cristian Hoyos <choyos@frba.utn.edu.ar>
2026-04-29 00:11:14 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/holamundo.cpp
@@ -0,0 +1,9 @@
+/* holamundo.cpp */
+
+#include <iostream>
+int main(){
+ std::cout << "Hello, World! 2026\n";
+ std::cout << "Nombre y Apellido: Cristian Hoyos\n";
+std::cout << "Curso AyED: K1053\n";
+std::cout << "Grupo: 2\n";
+}
